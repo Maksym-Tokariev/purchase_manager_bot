3da5c24e24f50f2f81270639eefce11edd6ac1be
Maks <paranoid063@gmail.com>
2026-01-31 23:41:00 +0200

    feat: MongoService.ts was created to store input data to MongoDB

--- net/services/DataCollector.ts
+++ net/services/DataCollector.ts
@@ -3,6 +3,7 @@ import {Purchase} from "../Components/Purchase";
 class DataCollector {
     private static data: Purchase[] = [];
 
+
     public static async collect(purchases: Purchase[]): Promise<void> {
         for (const pur of purchases) {
             console.log(`Purchase added : ${pur.name} | ${pur.price}`);
---
+++ net/services/MongoService.ts
@@ -0,0 +1,54 @@
+import {Collection, Db, DeleteResult, MongoClient, WithId} from "mongodb";
+import {Purchase} from "../Components/Purchase";
+
+export class MongoService {
+    private client: MongoClient;
+    private db?: Db;
+    private purchases?: Collection<Purchase>
+
+    constructor(private uri: string, private dbName: string) {
+        this.client = new MongoClient(uri);
+    }
+
+    public async connect(): Promise<void> {
+        try {
+            await this.client.connect();
+            this.db = this.client.db(this.dbName);
+            this.purchases = this.db.collection<Purchase>("purchases");
+            console.log(`Connect to db: ${this.dbName}`);
+        } catch (err) {
+            console.log("Db connection error: ", err);
+            throw err;
+        }
+    }
+
+    public async disconnect(): Promise<void> {
+        await this.client.close();
+        console.log("Connected has been closed");
+    }
+
+    public async insert(purchase: Purchase): Promise<void> {
+        if (!this.purchases) throw new Error("No purchases collection");
+        await this.purchases.insertOne(purchase);
+    }
+
+    public async find(query: Partial<Purchase>): Promise<WithId<Purchase>[]> {
+        if (!this.purchases) throw new Error("No purchases collection");
+        return await this.purchases.find(query).toArray();
+    }
+
+    public async update(id: string, update: Partial<Purchase>): Promise<number> {
+        if (!this.purchases) throw new Error("No purchases collection");
+        const res = await this.purchases.updateOne(
+            {_id: new Object(id)},
+            {$set: update}
+        );
+        return res.modifiedCount;
+    }
+
+    public async delete(id: string): Promise<number> {
+        if (!this.purchases) throw new Error("No purchases collection");
+        const res: DeleteResult = await this.purchases.deleteOne({_id: new Object(id)});
+        return res.deletedCount;
+    }
+}
